fb89c83e5cc6820ca13e0543a178873fd7d55e4d
dengyapeng <hitsjt@gmail.com>
2019-06-03 17:47:23 +0800

    second commit

---
+++ head.h
@@ -0,0 +1,29 @@
+#ifndef __HEAD_H
+#define __HEAD_H
+#define LISTENQ 1024
+#define MAXLINE 4096
+#define BUFFSIZE 8192
+#define SERV_PORT 9877
+#define SERV_PORT_STR "9877"
+#define SA struct sockaddr
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/time.h>
+#include<time.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<netdb.h>
+#include<signal.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/stat.h>
+#include<sys/uio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<sys/un.h>
+
+#endif
+
---
+++ tcpserv01.c
@@ -0,0 +1,42 @@
+#include "head.h"
+void str_echo(int sockfd)
+{
+	ssize_t n;
+	char buf[MAXLINE];
+again:
+	while((n=read(sockfd,buf,MAXLINE))>0)
+	{
+		write(sockfd,buf,n);
+		if(n<0)
+		goto again;
+		else if(n<0)
+		printf("str_echo");
+	}
+}
+int main(int argc,char **argv)
+{
+	int listenfd,connfd;
+	pid_t childpid;
+	socklen_t clilen;
+	struct sockaddr_in cliaddr,servaddr;
+
+	listenfd = socket(AF_INET,SOCK_STREAM,0);
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family=AF_INET;
+	servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	servaddr.sin_port=htons(SERV_PORT);
+	bind(listenfd,(SA*)&servaddr,sizeof(servaddr));
+	listen(listenfd,LISTENQ);
+	for(;;)
+	{
+		clilen = sizeof(cliaddr);
+		connfd=accept(listenfd,(SA*)&cliaddr,&clilen);
+		if((childpid=fork())==0)
+		{
+			close(listenfd);
+			str_echo(connfd);
+			exit(0);
+		}
+		close(connfd);
+	}
+}
